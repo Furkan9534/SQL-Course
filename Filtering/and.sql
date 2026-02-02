cf1ba3502e5fee876d7c6ec3f5317ab208aca192
Furkan KALKAN <furkankalkan2023@gmail.com>
2026-02-02 07:56:27 +0300

    AND

--- Filtering/and.sql
+++ Filtering/and.sql
@@ -8,7 +8,10 @@ WHERE condition1 AND condition2 AND condition3 ...;
 
 ---The AND operator displays a record if all the conditions are TRUE.
 
----Example
+---Example 1
 --Select all customers from Spain that starts with the letter 'F'
 SELECT *FROM Customers WHERE Country = 'Spain' AND CustomerName LIKE 'F%';
 
+---Example 2 
+SELECT * FROM Customers WHERE Country = 'Brazil' AND City = 'Rio de Janeiro'
+AND CustomerID > 50;
